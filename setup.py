cefd508a97544300c5790301659c0bbd972254a2
Emmanuel Serie <eserie@gmail.com>
2017-03-26 17:45:31 +0200

    specify requirements

--- setup.py
+++ setup.py
@@ -1,6 +1,15 @@
 from setuptools import setup, find_packages
 from persist import __version__ as VERSION
 
+
+def get_requirements():
+    req = open('requirements.txt').readlines()
+    req = map(lambda c: c.rstrip('\n'), req)
+    return req
+
+
+REQUIREMENTS = get_requirements()
+
 setup(name='persist',
       version=VERSION,
       description='Implement persistent collections for dask',
@@ -9,4 +18,5 @@ setup(name='persist',
       author_email='eserie@gmail.com',
       license='',
       packages=find_packages(),
+      install_requires=REQUIREMENTS,
       )
